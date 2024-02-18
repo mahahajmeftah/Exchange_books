05df7dbc6fb3ad768003b1e50843cbc3c419c0e0
Faustine G <faustine.guillou@etud.univ-angers.fr>
2024-02-18 22:17:09 +0100

    bouton delete

--- Front/src/components/Book_myBooks.js
+++ Front/src/components/Book_myBooks.js
@@ -2,10 +2,22 @@
 import React from 'react';
 import '../styles/MyBookcomponent.css'; // Ensure this is the correct path to your CSS file
 import Header from './Header';
+import axios from 'axios';
 
 const Book_myBooks = ({ book}) => {
 
-  console.log("book", book);
+  
+
+  const handleDelete = () => {
+    // Navigate to the borrow page
+    axios.delete(`http://localhost:5555/books/${book._id}`)
+            .then(() => {
+                window.alert('Updated');
+            })
+            .catch(error => console.err(error));
+    };
+
+
     
     const base64String= String.fromCharCode(...new Uint8Array(book.image.data.data));
     const contentType = book.image.contentType;
@@ -18,6 +30,7 @@ const Book_myBooks = ({ book}) => {
           <h3 className="mybook-title">{book.title}</h3>
           <p className="author">{book.author}</p>
           <p className="genre">{book.genre}</p>
+          <button className="delete-button" onClick={handleDelete}>Supprimer</button>
         </div>
       </div>
       
